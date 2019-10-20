86af1966484385b25b30265c5bb4151e1e42fa66
F1r <32818831+F1r@users.noreply.github.com>
2019-10-20 23:45:18 +0800

    Update update_io.py

--- update_io.py
+++ update_io.py
@@ -34,24 +34,17 @@ def UpdateREADME(direct):
     return content_new 
 
 def yml_add_new_item(origin, pathes, direct):
-    #print pathes
-    if len(pathes) == 1:
+    if len(pathes) == 1 and type(origin) == str:
+        return "{file_name}.md".format(file_name = direct)
+        
+    if len(pathes) == 1 and type(origin) == dict:
         new_node = {}
         new_node[pathes[0]] = "{file_name}.md".format(file_name = direct)
         return new_node
-    
+        
     if type(origin) == dict:
         if pathes[0] in origin.keys():
-            for i in range(0, len(origin[pathes[0]])):
-                val = yml_add_new_item(origin[pathes[0]][i], pathes[1:], direct)
-                if val == False:
-                    continue
-                else:
-                    origin[pathes[0]][i] = val
-                    return origin
-            new_node = {}
-            new_node[pathes[1]] = []
-            origin[pathes[0]].append(yml_add_new_item(new_node, pathes[1:], direct))
+            origin[pathes[0]] = yml_add_new_item(origin[pathes[0]], pathes[1:], direct)
             return origin
         else:
             return False
@@ -64,10 +57,18 @@ def yml_add_new_item(origin, pathes, direct):
             else:
                 origin[i] = val
                 return origin
-        new_node = {}
-        new_node[pathes[0]] = []
-        origin.append(new_node)
-        return origin
+        if len(pathes) >= 2:
+            new_node = {}
+            new_node[pathes[0]] = []
+            new_node[pathes[0]] = yml_add_new_item(new_node[pathes[0]], pathes[1:], direct)
+            origin.append(new_node)
+            return origin
+        elif len(pathes) == 1:
+            new_node = {}
+            new_node[pathes[0]] = ""
+            new_node[pathes[0]] = yml_add_new_item(new_node[pathes[0]], pathes, direct)
+            origin.append(new_node)
+            return origin
 
 def UpdateMKDOCS(direct):
     mkdocs_file = open(os.path.join(io_path, "mkdocs.yml"), "r")
@@ -81,6 +82,7 @@ def UpdateMKDOCS(direct):
     key_name = mkdocs_content_old_yml.keys()[0]
     mkdocs_content_old_yml[key_name] = yml_add_new_item(path_init, direct.split("/"), direct)
     mkdocs_content_new = mkdocs_content_old_before + dump(mkdocs_content_old_yml)
+    print dump(mkdocs_content_old_yml)
     mkdocs_file = open(os.path.join(io_path, "mkdocs.yml"), "w")
     mkdocs_file.write(mkdocs_content_new)
     mkdocs_file.close()
@@ -100,9 +102,9 @@ def Copy2IO(direct):
     doc_file_name = os.path.join(docs_path, os.path.dirname(direct))
     doc_file_name = os.path.join(doc_file_name, direct.split("/")[-1])
     print doc_file_name + ".md"
-    new_doc = open(doc_file_name + ".md", "w")
-    new_doc.write(UpdateREADME(direct))
-    new_doc.close()
+    #new_doc = open(doc_file_name + ".md", "w")
+    #new_doc.write(UpdateREADME(direct))
+    #new_doc.close()
     UpdateMKDOCS(direct)
 
 def Usage():
@@ -135,3 +137,4 @@ if __name__ == "__main__":
                 while direct.endswith("/"):
                     direct = direct[:-1]
                 Copy2IO(direct)
+                print " ------"
